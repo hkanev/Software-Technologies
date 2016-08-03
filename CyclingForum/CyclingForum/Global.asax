214128330c34fdb60250e05a9c0edb5ffcab244c
hkanev <hristiankanev@gmail.com>
2016-08-04 00:25:44 +0300

    TeamWork
    Generating  DB model first code and CRUD for posts and comments

---
+++ CyclingForum/CyclingForum/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CyclingForum.MvcApplication" Language="C#" %>
---
+++ CyclingForum/CyclingForum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CyclingForum.Startup))]
+namespace CyclingForum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
